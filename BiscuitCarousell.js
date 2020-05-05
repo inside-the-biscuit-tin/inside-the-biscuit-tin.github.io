f8b3c1a1173e477dd3928343f5029276f429a624
samjtwestlake <samjtwestlake@gmail.com>
2020-05-05 19:44:42 +0100

    updated

--- BiscuitCarousell.js
+++ BiscuitCarousell.js
@@ -1,25 +1,27 @@
+initialiseWidget({imageList:"",occasionList:""});
+
 function initialiseWidget(data){
     //variable exists, do what you want
     var imageList = data.imageList;
     var occasionList = data.occasionList;
 
-// biscuit_fp = "https://inside-the-biscuit-tin.github.io/Biscuits/"
-// imageList = [biscuit_fp + "Biscuits1.png",
-//         biscuit_fp + "Biscuits4.png",
-//         biscuit_fp + "Biscuits3_both.png",
-//         biscuit_fp + "Biscuits1.png",
-//         biscuit_fp + "Biscuits4.png",
-//         biscuit_fp + "Biscuits3_both.png",
-//         biscuit_fp + "Biscuits1.png"];
+biscuit_fp = "https://inside-the-biscuit-tin.github.io/Biscuits/"
+imageList = [biscuit_fp + "Biscuits1.png",
+        biscuit_fp + "Biscuits4.png",
+        biscuit_fp + "Biscuits3_both.png",
+        biscuit_fp + "Biscuits1.png",
+        biscuit_fp + "Biscuits4.png",
+        biscuit_fp + "Biscuits3_both.png",
+        biscuit_fp + "Biscuits1.png"];
 
    letters_fp = "https://inside-the-biscuit-tin.github.io/Letters/"
-// occasionList = ["Epidemics",
-//             "Moving",
-//             "Epidemics",
-//             "Epidemics",
-//             "Moving",
-//             "Epidemics",
-//             "Epidemics"];
+occasionList = ["Epidemics",
+            "Moving",
+            "Epidemics",
+            "Epidemics",
+            "Moving",
+            "Epidemics",
+            "Epidemics"];
 
     im_coords = [{left: "210px", top: "580px"},
                 {left: "135px", top: "390px"},
